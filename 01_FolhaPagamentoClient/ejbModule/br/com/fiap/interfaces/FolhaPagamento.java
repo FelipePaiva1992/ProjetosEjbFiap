ded7553670407166294e8ec16685c22be04c9da4
Felipe Paiva <spaiva.felipe@gmail.com>
2016-08-17 20:47:24 -0300

    Codigo fonte Exercicio 1 e 2 de EJB

---
+++ 01_FolhaPagamentoClient/ejbModule/br/com/fiap/interfaces/FolhaPagamento.java
@@ -0,0 +1,11 @@
+package br.com.fiap.interfaces;
+
+public interface FolhaPagamento {
+	void setSalario(double salario);
+
+	void setDesconto(double taxa);
+
+	double calcularINSS();
+
+	double calcularSalarioLiquido();
+}
